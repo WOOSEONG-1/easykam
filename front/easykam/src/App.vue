20e309ccba7932927bfd35a594b852dcbe88e108
WOOSEONG-1 <asdf1916@naver.com>
2025-08-12 23:21:23 +0900

    fix

--- front/easykam/src/App.vue
+++ front/easykam/src/App.vue
@@ -2,7 +2,7 @@
   <main style="max-width:720px;margin:40px auto;font:16px/1.5 system-ui">
     <img src="./assets/kamco.gif" alt="Easykam Image" style="max-width: 350px;" />s
     <h1>easyKam</h1>
-    <button @click="callApi">Click Me!</button>
+    <button @click="callApi">Click Me!!</button>
     <input v-model="name" placeholder="name" style="margin-left:8px"/>
     <pre v-if="resp" style="background:#111;color:#0f0;padding:8px;border-radius:8px;white-space:pre-wrap">{{ resp }}</pre>
   </main>
